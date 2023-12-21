c19f50a54da01ed1e911598241e54f57606de897
Reked37 <dtruesdale37@gmail.com>
2023-12-20 19:51:15 -0600

    done

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,19 @@
 function selectionSort(arr) {
-  // type your code here
+  let n = arr.length
+  for(i=0;i<n-1;i++){
+    let minIndex = i
+    for(j=i+1;j<n;j++){
+      if(arr[j]<arr[minIndex]){
+        minIndex= j
+      }
+    }
+    if(minIndex !== i){
+      let temp = arr[i]
+      arr[i]= arr[minIndex]
+      arr[minIndex] = temp
+    }
+  }
+  return arr
 }
 
 if (require.main === module) {
